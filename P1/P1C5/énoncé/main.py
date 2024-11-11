fe3493c3c5a62d7b88cbfef1540d0f795f93e3f4
flo <boireaufloriane@hotmail.fr>
2024-11-11 17:14:36 +0100

    exercice 3 reussi

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,14 @@
-# Écrivez votre code ici !
+#Définissez une variable  nom  contenant votre nom sous forme de chaîne de caractères.
+nom = "floriane"
+#Définissez une variable  age  contenant votre âge sous forme d'un entier.
+age = 35
+#Définissez une variable  taille  contenant votre taille sous forme d'un nombre à virgule flottante.
+taille = 1.62
+#Définissez une variable  est_etudiant  contenant une valeur booléenne et indiquant si vous êtes étudiant ou non.
+question_etudiant = True
+print(f"je m'apelle {nom} ,j'ai {age} ans ,je mesure {taille} et c'est {question_etudiant}, je suis etudiante.")
+
+print(type(nom))
+print(type(age))
+print(type(taille))
+print(type(question_etudiant))
