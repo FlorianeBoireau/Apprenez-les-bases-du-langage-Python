4df1f199aef5944af2653acdcc213a333804c97f
flo <boireaufloriane@hotmail.fr>
2024-11-11 18:12:59 +0100

    exercice 4 reussi

--- P1/P1C6/énoncé/main.py
+++ P1/P1C6/énoncé/main.py
@@ -1,1 +1,20 @@
-# Écrivez votre code ici !
+#Créez une liste nommée  fruits  contenant les éléments  pomme  ,  banane  et  orange  .
+fruits = ["pomme","banane","orange"]
+#Ajoutez  kiwi  à la liste  fruits  .
+fruits.append("kiwi")
+print(fruits)
+
+#Supprimez  orange  de la liste  fruits  .
+fruits.remove("orange")
+print(fruits)
+
+#Modifiez le deuxième élément de la liste  fruits  en  ananas  .
+fruits[1] = "ananas"
+print(fruits)
+
+#Affichez la longueur de la liste  fruits  .
+print(len(fruits))
+
+#Triez la liste  fruits  par ordre alphabétique et affichez-la.
+fruits.sort() 
+print(fruits)
